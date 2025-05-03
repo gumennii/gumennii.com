c63b1903a9c5e1d7f44fd5e211075aaa940a85dc
Mihail <mihail.gumennii@gmail.com>
2025-05-02 22:07:19 -0500

    fix: favicons

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -5,7 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin")
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const BUILD_PATH = path.resolve(__dirname, '../build')
 
@@ -75,6 +76,14 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
       chunkFilename: '[id].[contenthash].css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/img/favicon/favicon-16x16.png', to: 'favicon-16x16.png' },
+        { from: './src/img/favicon/favicon-32x32.png', to: 'favicon-32x32.png' },
+        { from: './src/img/favicon/apple-touch-icon.png', to: 'apple-touch-icon.png' },
+        { from: './src/img/favicon/site.webmanifest', to: 'site.webmanifest' }
+      ]
     }),
   ],
 
